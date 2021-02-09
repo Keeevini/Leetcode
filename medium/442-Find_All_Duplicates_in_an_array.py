32659bede7fe99ab297a5d5cac010092677cc2be
Keeevini <kevin.ni2000@gmail.com>
2021-02-09 00:00:45 -0800

    442 python

---
+++ medium/442-Find_All_Duplicates_in_an_array.py
@@ -0,0 +1,9 @@
+def findDuplicates(self, nums: List[int]) -> List[int]:
+    ans = []
+    for index, val in enumerate(nums):
+        value = abs(val)
+        if nums[value-1] < 0:
+            ans.append(value)
+        else:
+            nums[value-1] = -nums[value-1]
+    return ans
