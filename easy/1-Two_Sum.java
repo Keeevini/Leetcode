ce3453ae49adb099279a3aba21fb7f5d559e2305
Keeevini <kevin.ni2000@gmail.com>
2023-06-08 18:31:15 -0700

    Reformatted easy/1

--- easy/1-Two_Sum.java
+++ easy/1-Two_Sum.java
@@ -1,17 +1,17 @@
 public int[] twoSum(int[] nums, int target) {
-		HashMap<Integer, Integer> m = new HashMap<>();
-		int[] ans = new int[2];
-		
-		for(int a = 0; a < nums.length; a++) {
-			if(m.containsKey(new Integer(target-nums[a]))) {
-				ans[0] = a;
-				ans[1] = m.get(new Integer(target-nums[a])).intValue();
-				break;
-			}
-			
-			m.put(new Integer(nums[a]), new Integer(a));
+	HashMap<Integer, Integer> m = new HashMap<>();
+	int[] ans = new int[2];
+	
+	for(int a = 0; a < nums.length; a++) {
+		if(m.containsKey(new Integer(target-nums[a]))) {
+			ans[0] = a;
+			ans[1] = m.get(new Integer(target-nums[a])).intValue();
+			break;
 		}
 		
-		return ans;
+		m.put(new Integer(nums[a]), new Integer(a));
+	}
+	
+	return ans;
 		
 }
--- easy/1-Two_Sum.py
+++ easy/1-Two_Sum.py
@@ -1,12 +1,12 @@
 def two_sum(self, nums: List[int], target: int) -> List[int]:
     ans = []
     d = {}
-    
+
     for index, val in enumerate(nums):
         if (target-val) in d:
             ans.append(index)
             ans.append(d[target-val])
             break
 
-    d[val] = index
+        d[val] = index
     return ans
